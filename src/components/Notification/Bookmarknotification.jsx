35f038bf58d25712b796a4a06a35d1d5c3e3f04d
Raihanroo <raihanroo21@gmail.com>
2023-12-23 17:39:02 +0300

    Eight commit

--- src/components/Notification/Bookmarknotification.jsx
+++ src/components/Notification/Bookmarknotification.jsx
@@ -3,7 +3,9 @@ const Bookmarknotification = ({blog_title, onClose}) => {
     return (
         <div className="mt-5 border-red-300 p-3 mb-4">
            <h2>Bookmark added for{blog_title}</h2> 
-          
+           <div>
+            <button onClick={onClose}>close</button>
+           </div>
         </div>
     );
 };
