18489d96af3ff6cc88ad2f3e3420f90cef0c4ba6
Raihan <113172842+Raihanroo@users.noreply.github.com>
2023-12-26 13:03:22 +0300

    Update Tecno.jsx

--- src/components/Tecno/Tecno.jsx
+++ src/components/Tecno/Tecno.jsx
@@ -4,16 +4,38 @@ import Service from '../Service/Service';
 import { useReadingContext } from '../Context/ReadingContext';
 import Bookmarknotification from '../Notification/Bookmarknotification';
 const Tecno = () => {
-    const [services, setServices] = useState([]);
+    // const [services, setServices] = useState([]);
     const {getReadaingTime, getBookmark} = useReadingContext() ;
     
 
-    useEffect(() =>{
-        fetch('public/data.json')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    },[])
-
+    // useEffect(() =>{
+    //     fetch('public/data.json')
+    //     .then(res => res.json())
+    //     .then(data => setServices(data))
+    // },[])
+    const services = [
+  {
+    id: "64265c430416a59b7065b5d9",
+    author_picture: "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    blog_picture: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    read_time: 6,
+    author_name: "Dejesus Holland",
+    blog_title: "By programming yourself, how to get better at programming.",
+    about: "Sit sit laborum pariatur consectetur dolor excepteur commodo Lorem amet et cillum tempor proident. Fugiat consectetur deserunt proident velit sit reprehenderit ipsum aute sit ullamco. Ipsum voluptate tempor enim est sunt tempor elit duis commodo in exercitation. Excepteur qui culpa exercitation reprehenderit exercitation ipsum magna consequat nisi.\r\n",
+    date: "March 15, 2023"
+  },
+  {
+    id: "64265c43879b78edcd9a2a40",
+    author_picture: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    blog_picture: "https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    read_time: 9,
+    author_name: "Cecelia Woodward",
+    blog_title: "How to get your first job as a self-taught programmer",
+    about: "Amet minim excepteur elit tempor aliquip commodo. Fugiat officia sunt laborum consequat magna. Enim ullamco reprehenderit nulla mollit labore laboris.\r\n",
+    date: "February 27, 2023"
+  },
+  // ... (repeat for the rest of the objects)
+];
 
 
     return (
